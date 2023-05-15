82952996ef0c022277d0877c562655b76a26aedb
Saikat Chakraborty <saikatgpae@gmail.com>
2023-05-15 21:39:51 +0530

    Add store and logo.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
+const store = configureStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+// const store = configureStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
