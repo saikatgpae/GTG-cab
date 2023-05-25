31aab09b6599f9162bed426c0e79a43c9263e08b
Saikat Chakraborty <saikatgpae@gmail.com>
2023-05-25 18:03:02 +0530

    Remove linters.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware } from 'redux';
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer';
