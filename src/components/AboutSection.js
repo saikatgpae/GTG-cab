4df0bb25576075c72f4dfe5d6b268c90baf4aab8
Saikat Chakraborty <saikatgpae@gmail.com>
2024-11-09 21:57:26 +0530

    Remove unnecessary commented lines.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -14,11 +14,6 @@ export default function AboutSection() {
           <h4 className="about-section__left-title">
             A ride is always available for you 24/7
           </h4>
-          {/* <p className="about-section__desc">
-            We are a car rental service available to you 24/7. We provide the most
-            confortable and cheapest rides in the country. Hook us up for your trips and
-            get the best confort can offer.
-          </p> */}
           <p>
             <a href="#register" className="btn btn-oval">
               Learn more
